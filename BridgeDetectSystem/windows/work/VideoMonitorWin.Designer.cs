7225e087639fa751e4d866f5ce655bfd1fe704c6
wyj <myrljw@163.com>
2019-01-10 20:46:31 +0800

    增加界面摄像头控制功能

--- BridgeDetectSystem/windows/work/VideoMonitorWin.Designer.cs
+++ BridgeDetectSystem/windows/work/VideoMonitorWin.Designer.cs
@@ -36,6 +36,7 @@
             this.btnVideo2 = new System.Windows.Forms.ToolStripMenuItem();
             this.btnVideo3 = new System.Windows.Forms.ToolStripMenuItem();
             this.btnVideo4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnVideo5 = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel7 = new System.Windows.Forms.Panel();
@@ -50,6 +51,18 @@
             this.txtRailway = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.panelmax = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.picBox5 = new System.Windows.Forms.PictureBox();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.auto = new System.Windows.Forms.Button();
+            this.right = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.Button();
+            this.down = new System.Windows.Forms.Button();
+            this.up = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -62,6 +75,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            this.panelmax.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel10.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox5)).BeginInit();
+            this.panel9.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -74,10 +92,11 @@
             this.btnVideo1,
             this.btnVideo2,
             this.btnVideo3,
-            this.btnVideo4});
+            this.btnVideo4,
+            this.btnVideo5});
             this.menuStrip1.Location = new System.Drawing.Point(22, 72);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1108, 48);
+            this.menuStrip1.Size = new System.Drawing.Size(1745, 48);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -126,14 +145,23 @@
             this.btnVideo4.Text = "4放大";
             this.btnVideo4.Click += new System.EventHandler(this.btnVideo4_Click);
             // 
+            // btnVideo5
+            // 
+            this.btnVideo5.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnVideo5.ForeColor = System.Drawing.Color.White;
+            this.btnVideo5.Name = "btnVideo5";
+            this.btnVideo5.Size = new System.Drawing.Size(71, 44);
+            this.btnVideo5.Text = "5放大";
+            this.btnVideo5.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
             // panel1
             // 
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(22, 120);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1108, 778);
+            this.panel1.Size = new System.Drawing.Size(1025, 765);
             this.panel1.TabIndex = 1;
             // 
             // panel3
@@ -143,16 +171,16 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(0, 389);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1108, 389);
+            this.panel3.Size = new System.Drawing.Size(1025, 376);
             this.panel3.TabIndex = 1;
             // 
             // panel7
             // 
             this.panel7.Controls.Add(this.picBox4);
             this.panel7.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel7.Location = new System.Drawing.Point(554, 0);
+            this.panel7.Location = new System.Drawing.Point(510, 0);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(554, 389);
+            this.panel7.Size = new System.Drawing.Size(515, 376);
             this.panel7.TabIndex = 1;
             // 
             // picBox4
@@ -160,7 +188,7 @@
             this.picBox4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.picBox4.Location = new System.Drawing.Point(0, 0);
             this.picBox4.Name = "picBox4";
-            this.picBox4.Size = new System.Drawing.Size(554, 389);
+            this.picBox4.Size = new System.Drawing.Size(515, 376);
             this.picBox4.TabIndex = 0;
             this.picBox4.TabStop = false;
             // 
@@ -170,7 +198,7 @@
             this.panel6.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel6.Location = new System.Drawing.Point(0, 0);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(554, 389);
+            this.panel6.Size = new System.Drawing.Size(510, 376);
             this.panel6.TabIndex = 0;
             // 
             // picBox3
@@ -178,7 +206,7 @@
             this.picBox3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.picBox3.Location = new System.Drawing.Point(0, 0);
             this.picBox3.Name = "picBox3";
-            this.picBox3.Size = new System.Drawing.Size(554, 389);
+            this.picBox3.Size = new System.Drawing.Size(510, 376);
             this.picBox3.TabIndex = 0;
             this.picBox3.TabStop = false;
             // 
@@ -189,16 +217,16 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1108, 389);
+            this.panel2.Size = new System.Drawing.Size(1025, 389);
             this.panel2.TabIndex = 0;
             // 
             // panel5
             // 
             this.panel5.Controls.Add(this.picBox2);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel5.Location = new System.Drawing.Point(554, 0);
+            this.panel5.Location = new System.Drawing.Point(510, 0);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(554, 389);
+            this.panel5.Size = new System.Drawing.Size(515, 389);
             this.panel5.TabIndex = 1;
             // 
             // picBox2
@@ -206,7 +234,7 @@
             this.picBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.picBox2.Location = new System.Drawing.Point(0, 0);
             this.picBox2.Name = "picBox2";
-            this.picBox2.Size = new System.Drawing.Size(554, 389);
+            this.picBox2.Size = new System.Drawing.Size(515, 389);
             this.picBox2.TabIndex = 0;
             this.picBox2.TabStop = false;
             // 
@@ -216,7 +244,7 @@
             this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel4.Location = new System.Drawing.Point(0, 0);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(554, 389);
+            this.panel4.Size = new System.Drawing.Size(510, 389);
             this.panel4.TabIndex = 0;
             // 
             // picBox1
@@ -224,7 +252,7 @@
             this.picBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.picBox1.Location = new System.Drawing.Point(0, 0);
             this.picBox1.Name = "picBox1";
-            this.picBox1.Size = new System.Drawing.Size(554, 389);
+            this.picBox1.Size = new System.Drawing.Size(510, 389);
             this.picBox1.TabIndex = 0;
             this.picBox1.TabStop = false;
             // 
@@ -232,7 +260,7 @@
             // 
             this.txtRailway.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.txtRailway.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.txtRailway.Location = new System.Drawing.Point(979, 81);
+            this.txtRailway.Location = new System.Drawing.Point(1616, 81);
             this.txtRailway.Name = "txtRailway";
             this.txtRailway.Size = new System.Drawing.Size(103, 30);
             this.txtRailway.TabIndex = 2;
@@ -240,28 +268,160 @@
             // timer1
             // 
             this.timer1.Enabled = true;
-            //this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label1
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Honeydew;
-            this.label1.Location = new System.Drawing.Point(828, 86);
+            this.label1.Location = new System.Drawing.Point(1465, 86);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(125, 19);
             this.label1.TabIndex = 3;
             this.label1.Text = "主桁距离(cm)";
             // 
+            // panelmax
+            // 
+            this.panelmax.Controls.Add(this.panel8);
+            this.panelmax.Controls.Add(this.panel1);
+            this.panelmax.Location = new System.Drawing.Point(22, 133);
+            this.panelmax.Name = "panelmax";
+            this.panelmax.Size = new System.Drawing.Size(1745, 765);
+            this.panelmax.TabIndex = 4;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.panel10);
+            this.panel8.Controls.Add(this.panel9);
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel8.Location = new System.Drawing.Point(1025, 0);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(720, 765);
+            this.panel8.TabIndex = 2;
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.picBox5);
+            this.panel10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel10.Location = new System.Drawing.Point(0, 0);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(720, 564);
+            this.panel10.TabIndex = 1;
+            // 
+            // picBox5
+            // 
+            this.picBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picBox5.Location = new System.Drawing.Point(0, 0);
+            this.picBox5.Name = "picBox5";
+            this.picBox5.Size = new System.Drawing.Size(720, 564);
+            this.picBox5.TabIndex = 0;
+            this.picBox5.TabStop = false;
+            // 
+            // panel9
+            // 
+            this.panel9.Controls.Add(this.label2);
+            this.panel9.Controls.Add(this.comboBox1);
+            this.panel9.Controls.Add(this.auto);
+            this.panel9.Controls.Add(this.right);
+            this.panel9.Controls.Add(this.left);
+            this.panel9.Controls.Add(this.down);
+            this.panel9.Controls.Add(this.up);
+            this.panel9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel9.Location = new System.Drawing.Point(0, 0);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(720, 765);
+            this.panel9.TabIndex = 0;
+            this.panel9.Paint += new System.Windows.Forms.PaintEventHandler(this.panel9_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(483, 684);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "云台速度：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.comboBox1.Location = new System.Drawing.Point(612, 681);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(95, 26);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // auto
+            // 
+            this.auto.Location = new System.Drawing.Point(201, 644);
+            this.auto.Name = "auto";
+            this.auto.Size = new System.Drawing.Size(104, 30);
+            this.auto.TabIndex = 4;
+            this.auto.Text = "AUTO";
+            this.auto.UseVisualStyleBackColor = true;
+            this.auto.Click += new System.EventHandler(this.auto_Click);
+            // 
+            // right
+            // 
+            this.right.Location = new System.Drawing.Point(363, 644);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(90, 30);
+            this.right.TabIndex = 3;
+            this.right.Text = "RIGHT";
+            this.right.UseVisualStyleBackColor = true;
+            this.right.Click += new System.EventHandler(this.right_Click);
+            this.right.MouseDown += new System.Windows.Forms.MouseEventHandler(this.right_MouseDown);
+            this.right.MouseUp += new System.Windows.Forms.MouseEventHandler(this.right_MouseUp);
+            // 
+            // left
+            // 
+            this.left.Location = new System.Drawing.Point(40, 644);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(91, 30);
+            this.left.TabIndex = 2;
+            this.left.Text = "LEFT";
+            this.left.UseVisualStyleBackColor = true;
+            this.left.MouseDown += new System.Windows.Forms.MouseEventHandler(this.left_MouseDown);
+            this.left.MouseUp += new System.Windows.Forms.MouseEventHandler(this.left_MouseUp);
+            // 
+            // down
+            // 
+            this.down.Location = new System.Drawing.Point(201, 708);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(104, 30);
+            this.down.TabIndex = 1;
+            this.down.Text = "DOWN";
+            this.down.UseVisualStyleBackColor = true;
+            this.down.MouseDown += new System.Windows.Forms.MouseEventHandler(this.down_MouseDown);
+            this.down.MouseUp += new System.Windows.Forms.MouseEventHandler(this.down_MouseUp);
+            // 
+            // up
+            // 
+            this.up.Location = new System.Drawing.Point(201, 582);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(104, 37);
+            this.up.TabIndex = 0;
+            this.up.Text = "UP";
+            this.up.UseVisualStyleBackColor = true;
+            this.up.MouseDown += new System.Windows.Forms.MouseEventHandler(this.up_MouseDown);
+            this.up.MouseUp += new System.Windows.Forms.MouseEventHandler(this.up_MouseUp);
+            // 
             // VideoMonitorWin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1152, 922);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtRailway);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1789, 922);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panelmax);
+            this.Controls.Add(this.txtRailway);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
@@ -290,6 +450,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
             this.panel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            this.panelmax.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel10.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox5)).EndInit();
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -317,5 +483,18 @@
         private System.Windows.Forms.TextBox txtRailway;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem btnVideo5;
+        private System.Windows.Forms.Panel panelmax;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Button right;
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.Button down;
+        private System.Windows.Forms.Button up;
+        private System.Windows.Forms.PictureBox picBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button auto;
     }
 }
